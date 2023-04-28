f703d1e7c10f8e7febdd586291b4391e939b6b27
mario andre <mario.andre.franco@gmail.com>
2023-04-28 11:50:07 -0300

    ajustando tela pegando da api de producao

--- src/views/TelaPlanner.vue
+++ src/views/TelaPlanner.vue
@@ -1,98 +1,102 @@
 <template>
-    <div class="bg-gradient-to-t from-lilac-bush-400 to-lilac-bush-800 h-screen flex justify-center">
+    <div class="bg-gradient-to-t from-lilac-bush-400 to-lilac-bush-800 h-auto flex justify-center">
         <div class="container flex flex-col mx-auto my-auto bg-lilac-bush-400 ">
-                <accordion title="Segunda-Feira" class="text-lg" >
-                    <div class="flex flex-col">
-                        <p class="pb-5 flex justify-start text-lilac-bush-50">João - 15:30 até 16:30</p> 
-                    </div>
-                    <div class="flex flex-col">
-                        <p class="pb-5 flex justify-start text-lilac-bush-50">Enzo - 17:00 até 18:00</p> 
-                    </div>
-                    <div class="flex flex-col">
-                        <p class="pb-5 flex justify-start text-lilac-bush-50">Ana Clara - 18:30 até 19:30</p> 
-                    </div>
-                    <div class="flex flex-col">
-                        <p class="pb-5 flex justify-start text-lilac-bush-50">Marianna - 20:00 até 21:30</p> 
+                <accordion title="Segunda-Feira" class="text-lg">
+                    <div class="flex flex-col" v-for="(aluno,index) in alunosSegunda" :key="index">
+                        <p class="pb-5 flex justify-start text-lilac-bush-50">{{aluno.nome}} - {{ aluno.horarioAula }}</p> 
                     </div>
                 </accordion>
-                <accordion title="Terça-Feira">
-                    <div class="flex flex-col">
-                        <p class="pb-5 flex justify-start text-lilac-bush-50">João - 15:30 até 16:30</p> 
-                    </div>
-                    <div class="flex flex-col">
-                        <p class="pb-5 flex justify-start text-lilac-bush-50">Enzo - 17:00 até 18:00</p> 
-                    </div>
-                    <div class="flex flex-col">
-                        <p class="pb-5 flex justify-start text-lilac-bush-50">Ana Clara - 18:30 até 19:30</p> 
-                    </div>
-                    <div class="flex flex-col">
-                        <p class="pb-5 flex justify-start text-lilac-bush-50">Marianna - 20:00 até 21:30</p> 
+                <accordion title="Terça-Feira" class="text-lg">
+                    <div class="flex flex-col" v-for="(aluno,index) in alunosTerca" :key="index">
+                        <div v-if="alunosTerca.length === 0">
+                            <p class="pb-5 flex justify-start text-lilac-bush-50">Nenhum aluno nesse dia</p>
+                        </div>
+                        <div v-else>
+                            <p class="pb-5 flex justify-start text-lilac-bush-50">{{aluno.nome}} - {{ aluno.horarioAula }}</p> 
+                        </div>    
                     </div>
                 </accordion>    
-                <accordion title="Quarta-Feira">
-                    <div class="flex flex-col">
-                        <p class="pb-5 flex justify-start text-lilac-bush-50">João - 15:30 até 16:30</p> 
+                <accordion title="Quarta-Feira" class="text-lg">
+                    <div class="flex flex-col" v-for="(aluno,index) in alunosQuarta" :key="index">
+                        <p class="pb-5 flex justify-start text-lilac-bush-50">{{aluno.nome}} - {{ aluno.horarioAula }}</p> 
                     </div>
-                    <div class="flex flex-col">
-                        <p class="pb-5 flex justify-start text-lilac-bush-50">Enzo - 17:00 até 18:00</p> 
-                    </div>
-                    <div class="flex flex-col">
-                        <p class="pb-5 flex justify-start text-lilac-bush-50">Ana Clara - 18:30 até 19:30</p> 
-                    </div>
-                    <div class="flex flex-col">
-                        <p class="pb-5 flex justify-start text-lilac-bush-50">Marianna - 20:00 até 21:30</p> 
-                    </div>
+                    
                 </accordion>  
-                <accordion title="Quinta-Feira">
-                    <div class="flex flex-col">
-                        <p class="pb-5 flex justify-start text-lilac-bush-50">João - 15:30 até 16:30</p> 
+                <accordion title="Quinta-Feira" class="text-lg">
+                    <div class="flex flex-col" v-for="(aluno,index) in alunosQuinta" :key="index">
+                        <p class="pb-5 flex justify-start text-lilac-bush-50">{{aluno.nome}} - {{ aluno.horarioAula }}</p> 
                     </div>
-                    <div class="flex flex-col">
-                        <p class="pb-5 flex justify-start text-lilac-bush-50">Enzo - 17:00 até 18:00</p> 
-                    </div>
-                    <div class="flex flex-col">
-                        <p class="pb-5 flex justify-start text-lilac-bush-50">Ana Clara - 18:30 até 19:30</p> 
-                    </div>
-                    <div class="flex flex-col">
-                        <p class="pb-5 flex justify-start text-lilac-bush-50">Marianna - 20:00 até 21:30</p> 
-                    </div>
+                    
                 </accordion>  
-                <accordion title="Sexta-Feira">
-                    <div class="flex flex-col">
-                        <p class="pb-5 flex justify-start text-lilac-bush-50">João - 15:30 até 16:30</p> 
+                <accordion title="Sexta-Feira" class="text-lg">
+                    <div class="flex flex-col" v-for="(aluno,index) in alunosSexta" :key="index">
+                        <p class="pb-5 flex justify-start text-lilac-bush-50">{{aluno.nome}} - {{ aluno.horarioAula }}</p> 
                     </div>
-                    <div class="flex flex-col">
-                        <p class="pb-5 flex justify-start text-lilac-bush-50">Enzo - 17:00 até 18:00</p> 
-                    </div>
-                    <div class="flex flex-col">
-                        <p class="pb-5 flex justify-start text-lilac-bush-50">Ana Clara - 18:30 até 19:30</p> 
-                    </div>
-                    <div class="flex flex-col">
-                        <p class="pb-5 flex justify-start text-lilac-bush-50">Marianna - 20:00 até 21:30</p> 
-                    </div>
+                    
                 </accordion>  
-                <accordion title="Sábado">
-                    <div class="flex flex-col">
-                        <p class="pb-5 flex justify-start text-lilac-bush-50">João - 15:30 até 16:30</p> 
+                <accordion title="Sábado" class="text-lg">
+                    <div class="flex flex-col" v-for="(aluno,index) in alunosSabado" :key="index">
+                        <p class="pb-5 flex justify-start text-lilac-bush-50">{{aluno.nome}} - {{ aluno.horarioAula }}</p> 
                     </div>
-                    <div class="flex flex-col">
-                        <p class="pb-5 flex justify-start text-lilac-bush-50">Enzo - 17:00 até 18:00</p> 
-                    </div>
-                    <div class="flex flex-col">
-                        <p class="pb-5 flex justify-start text-lilac-bush-50">Ana Clara - 18:30 até 19:30</p> 
-                    </div>
-                    <div class="flex flex-col">
-                        <p class="pb-5 flex justify-start text-lilac-bush-50">Marianna - 20:00 até 21:30</p> 
-                    </div>
+                    
                 </accordion>  
         </div>
     </div>
 </template>
 <script>
 import accordion from '../components/AccordionMenu.vue'
+import axios from 'axios'
 export default {
     components: {
         accordion
+    },
+    data () {
+        return {
+            alunos: [],
+            alunosSegunda: [],
+            alunosTerca: [],
+            alunosQuarta: [],
+            alunosQuinta: [],
+            alunosSexta: [],
+            alunosSabado: []
+        }
+    },
+    methods: {
+        formatarDiaDaSemana(payload) {
+            for (let index = 0; index < payload.length; index++) {
+                const element = payload[index];
+                if(element.diaSemana === "segunda-feira") {
+                   this.alunosSegunda.push(element) 
+                }
+                else if(element.diaSemana === "terça-feira") {
+                    this.alunosTerca.push(element) 
+                    console(this.alunosTerca.length)
+                }
+                else if(element.diaSemana === "quarta-feira") {
+                    this.alunosQuarta.push(element)
+                }
+                else if(element.diaSemana === "quinta-feira") {
+                    this.alunosQuinta.push(element)
+                }
+                else if(element.diaSemana === "sexta-feira") {
+                    this.alunosSexta.push(element)
+                }
+                else if(element.diaSemana === "sábado") {
+                    this.alunosSabado.push(element)
+                }
+
+                
+            }
+        }
+    },
+    mounted () {
+        axios.get('https://planner-backend-service.onrender.com/alunos')
+        .then(response => {
+            this.alunos = this.formatarDiaDaSemana(response.data)
+        })
+        .catch(error => {
+            console.log(error)
+        })
     }
 }
 </script>
